1357eb9e0569dc8651723aa6884573e002746ce1
VE-MUAH <vicentiaemuah21@gmail.com>
2026-02-02 16:28:53 +0000

    Update DEBUG setting to False in settings.py

--- my_first_project/my_first_project/settings.py
+++ my_first_project/my_first_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nb@+@2&z)%tgu$*%845mvg7$8hf!rogawcbob$b9-q2+u6xt4+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
